49f9c1aaa5d69cde49c7c863243000f8fd18f264
Jake Stanger <mail@jstanger.dev>
2024-05-22 21:41:40 +0100

    fix(bins): adjust to new api

--- src/bot/commands/home/bins.ts
+++ src/bot/commands/home/bins.ts
@@ -1,63 +1,87 @@
-import { Message, TextChannel } from "discord.js";
-import { ICommandMeta } from "../../../helper/commands/ICommand";
-import ConfigManager from "../../../manager/ConfigManager";
-import { JSDOM } from "jsdom";
-import { DateTime } from "luxon";
+import { Message } from 'discord.js';
+import { ICommandMeta } from '../../../helper/commands/ICommand';
+import ConfigManager from '../../../manager/ConfigManager';
+import { DateTime } from 'luxon';
 
-export async function getDates(url: string) {
-  const html = await fetch(url).then(r => r.text());
-  const document = new JSDOM(html).window.document;
+export async function getDates(url: string, uprn: string, usrn: string) {
+  const dates = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({ uprn, usrn }),
+  })
+    .then((r) => r.json())
+    .then((r) => r.dates)
+    .then(JSON.parse);
 
-  const recycling = document.querySelector(".recyclingBinDay .large").textContent;
-  const food = document.querySelector(".foodBinDay .large").textContent;
-  const general = document.querySelector(".blackBinDay .large").textContent;
-  const garden = document.querySelector(".gardenBinDay .large").textContent;
-
-  const format = "EEEE dd MMMM";
-  const recyclingDate = DateTime.fromFormat(recycling, format).plus({hours: 23});
-  const foodDate = DateTime.fromFormat(food, format).plus({hours: 23});
-  const generalDate = DateTime.fromFormat(general, format).plus({hours: 23});
-  const gardenDate = DateTime.fromFormat(garden, format).plus({hours: 23});
+  const generalDate = DateTime.fromISO(dates.blackBinDay[0]).set({hour: 23});
+  const foodDate = DateTime.fromISO(dates.foodBinDay[0]).set({hour: 23});
+  const gardenDate = DateTime.fromISO(dates.gardenBinDay[0]).set({hour: 23});
+  const recyclingDate = DateTime.fromISO(dates.recyclingBinDay[0]).set({hour: 23})
 
   return {
     recyclingDate,
     foodDate,
     generalDate,
-    gardenDate
+    gardenDate,
   };
 }
 
 async function bins(message: Message) {
-  const url = await ConfigManager.get().getById({
-    guildId: message.guild.id,
-    key: 'timer.home.bins.url'
-  }).then(c => c.value as string);
+  const configManager = ConfigManager.get();
 
-  if(url) {
-    const dates = await getDates(url);
-    const format = "EEE dd";
+  const url = await configManager
+    .getById({
+      guildId: message.guild.id,
+      key: 'timer.home.bins.apiUrl',
+    })
+    .then((c) => c.value as string);
+
+  const uprn = await configManager
+    .getById({
+      guildId: message.guild.id,
+      key: 'timer.home.bins.uprn',
+    })
+    .then((c) => c.value as string);
+
+  const usrn = await configManager
+    .getById({
+      guildId: message.guild.id,
+      key: 'timer.home.bins.usrn',
+    })
+    .then((c) => c.value as string);
+
+  if (url && uprn && usrn) {
+    const dates = await getDates(url, uprn, usrn);
+    const format = 'EEE dd';
 
     await message.channel.send({
       embed: {
         fields: [
           {
             name: ':wastebasket: General',
-            value: `**${dates.generalDate.toFormat(format)}** (${dates.generalDate.toRelative()})`,
+            value: `**${dates.generalDate.toFormat(
+              format
+            )}** (${dates.generalDate.toRelative()})`,
           },
           {
             name: ':recycle: Recycling',
-            value: `**${dates.recyclingDate.toFormat(format)}** (${dates.recyclingDate.toRelative()})`,
+            value: `**${dates.recyclingDate.toFormat(
+              format
+            )}** (${dates.recyclingDate.toRelative()})`,
           },
           {
             name: ':leaves: Garden',
-            value: `**${dates.gardenDate.toFormat(format)}** (${dates.gardenDate.toRelative()})`,
+            value: `**${dates.gardenDate.toFormat(
+              format
+            )}** (${dates.gardenDate.toRelative()})`,
           },
           {
             name: ':meat_on_bone: Food',
-            value: `**${dates.foodDate.toFormat(format)}** (${dates.foodDate.toRelative()})`,
+            value: `**${dates.foodDate.toFormat(
+              format
+            )}** (${dates.foodDate.toRelative()})`,
           },
-        ]
-      }
+        ],
+      },
     });
   }
 }
--- src/bot/timers/home/bins.ts
+++ src/bot/timers/home/bins.ts
@@ -1,7 +1,6 @@
 import { Guild, TextChannel } from 'discord.js';
 import ConfigManager from '../../../manager/ConfigManager';
 import { ITimerMetadata } from '../../../helper/timers/ITimer';
-import { JSDOM } from 'jsdom';
 import { DateTime } from 'luxon';
 import { getDates } from '../../commands/home/bins';
 
@@ -18,15 +17,29 @@ export async function bins(guild: Guild) {
   const url = await configManager
     .getById({
       guildId: guild.id,
-      key: 'timer.home.bins.url',
+      key: 'timer.home.bins.apiUrl',
     })
     .then((c) => c.value as string);
 
-  if (channelId && url) {
+  const uprn = await configManager
+    .getById({
+      guildId: guild.id,
+      key: 'timer.home.bins.uprn',
+    })
+    .then((c) => c.value as string);
+
+  const usrn = await configManager
+    .getById({
+      guildId: guild.id,
+      key: 'timer.home.bins.usrn',
+    })
+    .then((c) => c.value as string);
+
+  if (channelId && url && usrn && usrn) {
     const channel = (guild.channels.resolve(channelId) ??
       (await guild.client.channels.fetch(channelId))) as TextChannel;
 
-    const dates = await getDates(url);
+    const dates = await getDates(url, uprn, usrn);
 
     if (isTomorrow(dates.generalDate)) {
       await channel.send({
@@ -77,6 +90,8 @@ export const meta: ITimerMetadata = {
   help: 'Runs the bin checker daily',
   ensureConfig: {
     channel: '',
-    url: '',
+    apiUrl: '',
+    uprn: '',
+    usrn: ''
   },
 };
